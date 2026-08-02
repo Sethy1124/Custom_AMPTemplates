4a639d4c08ce4ef5d9ca508cc82f39a2faccc89b
Seth <47252832+Sethy1124@users.noreply.github.com>
2026-08-02 16:25:59 -0500

    Create run.sh to execute server with environment variables
    Add a bash script to run the server with specified ports.

---
+++ run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# save the script directory
+SCRIPT=$(readlink -f "$0")
+SCRIPTPATH=$(dirname "$SCRIPT")
+
+# set Ports
+export	TXHOST_FXS_PORT={{ServerPort}}
+export	TXHOST_TXA_PORT={{txPort}}
+
+# run server
+exec $SCRIPTPATH/alpine/opt/cfx-server/ld-musl-x86_64.so.1 \
+    --library-path "$SCRIPTPATH/alpine/usr/lib/v8/:$SCRIPTPATH/alpine/lib/:$SCRIPTPATH/alpine/usr/lib/" -- \
+    $SCRIPTPATH/alpine/opt/cfx-server/FXServer +set citizen_dir $SCRIPTPATH/alpine/opt/cfx-server/citizen/ $*
